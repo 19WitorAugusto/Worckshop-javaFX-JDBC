f2e43d86740900a4514e5c7bcd4481d410c7a970
witor augusto <witorsantosaugusto@gmail.com>
2020-06-17 09:18:33 -0300

    Remove Department

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,7 +1,14 @@
 package gui.util;
 
+import java.util.Optional;
+
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
+import model.entities.Department;
 
 public class Alerts {
 	public static void showAlert(String title, String header, String content, AlertType type) {
@@ -11,4 +18,13 @@ public class Alerts {
 		alert.setContentText(content);
 		alert.show();
 	}
+
+	public static Optional<ButtonType> showConfirmation(String title, String content) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
+	}
+
 }
